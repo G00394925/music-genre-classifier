bd9f56eb5e75faa414b7df729d13fb619ccf90d3
G00394925 <G00394925@atu.ie>
2025-02-19 13:32:44 +0000

    Start initial framework

---
+++ src/components/test.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const Test = (props) => {
+    
+    return (
+        <div>
+            <h1>Hello, this is test {props.case}.</h1>
+        </div>
+    )
+}
+
+export default Test;
