d82adf3f1bd196bd64bde8d5bf67f9eee21b0656
Macdarach Carty Joyce <145470957+G00394925@users.noreply.github.com>
2025-02-28 15:00:20 +0000

    Backend works -- Very simple example, remove later

--- backend/Server.py
+++ backend/Server.py
@@ -1,13 +1,15 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, jsonify
 from flask_cors import CORS
 import os
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods=['GET']) 
-def serve():
-    return send_from_directory(app.static_folder, 'index.html')
+# Test -- remove later
+@app.route('/api/test')
+def test():
+    return jsonify(message = 'Backend works')
+    
 
 @app.errorhandler(404)
 def not_found(e):
--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
 const Home = () => {
+    const [message, setMessage] = useState("")
+
+    useEffect(() => {
+        axios.get('/api/test')
+            .then(response => {
+                setMessage(response.data.message)
+            })
+            .catch(error => {
+                console.error("Error: ", error)
+            });
+    }, []);
+
     return(
         <div id="home-div">
             <h1 id="home-heading">Define my Track</h1>
             <p id="home-body">Got a song you like? Well it's time to find out what exactly you're listening to. 
             Use our music genre classifier to figure out what it is that makes your song sound so special</p>
+            <p>{message}</p>
         </div>     
     )
 }
