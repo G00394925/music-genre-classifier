136ead513bdc56c3303ba692aeb3c0db6b4ef0f1
Macdarach Carty Joyce <145470957+G00394925@users.noreply.github.com>
2025-03-03 11:00:32 +0000

    Dataset test

--- backend/Server.py
+++ backend/Server.py
@@ -1,15 +1,22 @@
 from flask import Flask, request, send_from_directory, jsonify
 from flask_cors import CORS
+import pandas as pd
+import numpy as np
+import librosa 
 import os
 
 app = Flask(__name__)
 CORS(app)
 
+def printData():
+    data = pd.read_csv('./data.csv')
+    print(data)   
+
 # Test -- remove later
 @app.route('/api/test')
 def test():
-    return jsonify(message = 'Backend works')
-    
+    printData()
+    return jsonify(message = 'Backend works') 
 
 @app.errorhandler(404)
 def not_found(e):
