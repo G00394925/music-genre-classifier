076001255162b27e541174faba9d964517a90756
G00394925 <G00394925@atu.ie>
2025-03-05 13:59:04 +0000

    Started on analyze page settings

--- src/components/analyze.js
+++ src/components/analyze.js
@@ -1,11 +1,38 @@
 import './analyze.css'
+import React, { useState } from 'react';
+import Select from 'react-select';
 
 const Analyze = () => {
+
+    const options = [[ 
+        { value: '1', label: 'Graph 1'},
+        { value: '2', label: 'Graph 2'},
+        { value: '3', label: 'Graph 3'}
+    ],
+    [
+        { value: 'blue', label: 'Blue'},
+        { value: 'red', label: 'Red'},
+        { value: 'green', label: 'Green'}
+    ]];
+
     return (
         <div id="main-div">
-            <div class="container">
-                <div class="left-div">
-                    Analyze page running
+            <div id="grid-div">
+                <div id="left-div">
+                    <h2 style={{fontWeight: "bolder"}}>Options</h2>
+                    <div id="options-div">
+                        <h5>Graph Type</h5>
+                        <Select options={options[0]} /> <br />
+
+                        <h5>Colour 1</h5>
+                        <Select options={options[1]} /> <br />
+
+                        <h5>Colour 2</h5>
+                        <Select options={options[1]} /> <br />
+
+                        <h5>Colour 3</h5>
+                        <Select options={options[1]} /> <br />
+                    </div>    
                 </div>
             </div>
         </div>
