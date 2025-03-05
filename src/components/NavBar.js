d2acbb425040dc3a95fd3491448de53be257b6a6
G00394925 <G00394925@atu.ie>
2025-03-05 13:06:49 +0000

    Further NavBar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,17 +2,20 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import './NavBar.css';
+import { useLocation } from 'react-router-dom';
 
 const NavBar = () => {
+    const location = useLocation();
+
     return (
         <Navbar>
             <Container className='nav-container'>
                 <Navbar.Brand href="/" id="logo">LOGO</Navbar.Brand>
                 <Nav className="ms-auto">
-                    <Nav.Link id="nav-item" href="/">Home</Nav.Link>
-                    <Nav.Link id="nav-item" href="/analyze">Analyze</Nav.Link>
-                    <Nav.Link id="nav-item" href="/">History</Nav.Link>
-                    <Nav.Link id="nav-item" href="/">Account</Nav.Link>
+                    <Nav.Link id="nav-item" href="/" className={location.pathname === '/' ? 'active' : 'inactive'}>Home</Nav.Link>
+                    <Nav.Link id="nav-item" href="/analyze" className={location.pathname === '/analyze' ? 'active' : 'inactive'}>Analyze</Nav.Link>
+                    <Nav.Link id="nav-item" href="/history" className={location.pathname === '/history' ? 'active' : 'inactive'}>History</Nav.Link>
+                    <Nav.Link id="nav-item" href="/account" className={location.pathname === '/account' ? 'active' : 'inactive'}>Account</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
