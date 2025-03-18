b63d2fb0586a8ff55dfdc31f2fe0e2f3445c88de
Macdarach Carty Joyce <145470957+G00394925@users.noreply.github.com>
2025-03-18 13:01:02 +0000

    Changed styling

--- src/components/home.js
+++ src/components/home.js
@@ -7,7 +7,8 @@ const Home = () => {
     return(
         <div id="home-div">
             <h1 id="home-heading">Define my Track</h1>
-            <p id="home-body">Got a song you like? Well it's time to find out what exactly you're listening to. 
+            <p id="home-body">Got a song you like? <br/> 
+            Well it's time to find out what exactly you're listening to. <br/> 
             Use our music genre classifier to figure out what it is that makes your song sound so special</p>
         </div>     
     )
