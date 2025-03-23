9275891c082ae8674903a27a4ed801c1f59a7c21
Macdarach Carty Joyce <145470957+G00394925@users.noreply.github.com>
2025-03-23 15:11:06 +0000

    Made some fields on registration page

--- src/components/home.js
+++ src/components/home.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/register');
+    }
 
     return(
         <div id="home-div">
@@ -10,7 +14,7 @@ const Home = () => {
             <p id="home-body">Got a song you like? <br/> 
             Well it's time to find out what exactly you're listening to. <br/> 
             Use our music genre classifier to figure out what it is that makes your song sound so special</p>
-            <button id="home-button" className="btn btn-primary">Get Started</button>
+            <button id="get-started-button" onClick={handleClick}>Get Started</button>
         </div>     
     )
 }
--- src/components/register.js
+++ src/components/register.js
@@ -1,10 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './register.css';
 import { useState } from 'react';
 
 const Register = () => {
+    const [userName, setUserName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [userName, setUserName] = useState('');
+    const [repeatPass, setRepeatPass] = useState('');
+    const [verifyPass, setVerifyPass] = useState(false);
 
     const validateEmail = (email) => {
         return String(email)
@@ -18,20 +21,60 @@ const Register = () => {
         <div>
             <form 
                 // onSubmit={handleSubmit} 
-                className="card m-4 mx-auto login-styling bg-secondary" 
-                style={{width: 400, padding: 25}}>
-                
+                className="card m-4 mx-auto login-styling" 
+                style={{width: 400, padding: 25, backgroundColor: '#171717', color: 'white', borderRadius: '15px'}}>
+
                 <div className="form-group">
-                    <label htmlFor="email">Email</label>
+                    <label>Username</label>
                     <input 
                         type="text" 
                         className="form-control" 
-                        style={{backgroundColor: '#7F7F7F', color: 'white', borderColor: '#7F7F7F'}} 
+                        style={{backgroundColor: '#2b2b2b', color: 'white', borderColor: '#7F7F7F'}} 
+                        value={userName} 
+                        onChange={(e) => setUserName(e.target.value)} 
+                    />
+                </div>
+                
+                <div className="form-group">
+                    <label>Email</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        style={{backgroundColor: '#2b2b2b', color: 'white', borderColor: '#7F7F7F'}} 
                         value={email} 
                         onChange={(e) => setEmail(e.target.value)} 
                     />
                 </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        style={{backgroundColor: '#2b2b2b', color: 'white', borderColor: '#7F7F7F'}} 
+                        value={password} 
+                        onChange={(e) => setPassword(e.target.value)} 
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="email">Repeat Password</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        style={{backgroundColor: '#2b2b2b', color: 'white', borderColor: '#7F7F7F'}} 
+                        value={repeatPass} 
+                        onChange={(e) => {
+                            setRepeatPass(e.target.value);
+                            setVerifyPass(password === e.target.value);
+                        }} 
+                    />
+                </div>
+
+                <input id="submit-button" type="submit" value="Register"/>
+                <label style={{ margin: '5px' }}>Already have an account? <a>Sign in here!</a></label>
             </form>
+
         </div>
     )
 }
