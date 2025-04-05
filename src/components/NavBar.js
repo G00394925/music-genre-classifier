938f80d76bcfcc4df9040ee78c9c6d0942baed33
Macdarach Carty Joyce <145470957+G00394925@users.noreply.github.com>
2025-04-05 11:51:07 +0000

    NavBar now changes if user is logged in

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,9 +3,11 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import './NavBar.css';
 import { useLocation } from 'react-router-dom';
+import { useAuth } from '../AuthProvider.js';
 
 const NavBar = () => {
     const location = useLocation();
+    const { user, logOut } = useAuth();
 
     return (
         <Navbar>
@@ -15,7 +17,11 @@ const NavBar = () => {
                     <Nav.Link id="nav-item" href="/" className={location.pathname === '/' ? 'active' : 'inactive'}>Home</Nav.Link>
                     <Nav.Link id="nav-item" href="/analyze" className={location.pathname === '/analyze' ? 'active' : 'inactive'}>Analyze</Nav.Link>
                     <Nav.Link id="nav-item" href="/history" className={location.pathname === '/history' ? 'active' : 'inactive'}>History</Nav.Link>
-                    <Nav.Link id="nav-item" href="/account" className={location.pathname === '/account' ? 'active' : 'inactive'}>Account</Nav.Link>
+                    {user ? (
+                        <Nav.Link id="nav-item" href="/account" className={location.pathname === '/account' ? 'active' : 'inactive'}>Account</Nav.Link>
+                    ) : (
+                        <Nav.Link id="nav-item" href="/login" className={location.pathname === '/login' ? 'active' : 'inactive'}>Log in</Nav.Link>
+                    )}
                 </Nav>
             </Container>
         </Navbar>
