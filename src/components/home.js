4e1e1be01b2837335855e12ff6126262dadee56e
Macdarach Carty Joyce <145470957+G00394925@users.noreply.github.com>
2025-04-08 13:18:01 +0000

    Changed up home page

--- src/components/home.js
+++ src/components/home.js
@@ -15,12 +15,14 @@ const Home = () => {
 
     return(
         <div id="home-div">
-            {/* <img className="spectrogram" src={spectrogram} alt="Spectrogram" /> */}
-            <h1 id="home-heading">Define my Track</h1>
-            <p id="home-body">Got a song you like? <br/> 
-            Well it's time to find out what exactly you're listening to. <br/> 
-            Use our music genre classifier to figure out what it is that makes your song sound so special</p>
+            <div id="welcome-message">
+                <h1 id="home-heading">Define my Track</h1>
+                <p id="home-body">Got a song you like? <br/> 
+                Well it's time to find out what exactly you're listening to. <br/> 
+                Use our music genre classifier to figure out what it is that makes your song sound so special</p>
 
+                <button id="get-started-button" onClick={handleClick}>Get Started</button>
+            </div>
             <div className="features-container">
                 <div className='feature-card'>
                     <h3>Genre Detection</h3>
@@ -34,7 +36,6 @@ const Home = () => {
                     <h3>Simple and Easy to use</h3>
                     <p>All you have to do is drag and drop your audio file to get started!</p>
                 </div>
-            <button id="get-started-button" onClick={handleClick}>Get Started</button>
             </div>
         </div>     
     )
